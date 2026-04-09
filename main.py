6a758b19c6a9a5d3de11a04558d75186cc99e5c5
JuanDeLosAndes <167449162+JuanDeLosAndes@users.noreply.github.com>
2026-04-09 16:12:05 +0000

    validacion de no repeticion de id hora y capacidad

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from typing import Optional
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
 app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
 
 class Clase(BaseModel):
     id: int
     nombre: str
     activa: bool
-    duracion: float
+    horario: float
     facultad: str
+    capacidad: int
+
+    @field_validator("horario")
+    def validar_horario(cls, value):
+        if value < 0:
+            raise ValueError("El horario no puede ser negativo")
+        return value
+
+    @field_validator("capacidad")
+    def validar_capacidad(cls, value):
+        if value < 0:
+            raise ValueError("La capacidad no puede ser negativa")
+        if value > 35:
+            raise ValueError("La capacidad no puede ser mayor a 35")
+        return value
+
 
 clases_db = []
 
@@ -19,18 +36,22 @@ clases_db = []
 def inicio():
     return {"mensaje": "API funcionando 🚀"}
 
-#get
 @app.get("/clases")
 def obtener_clases():
     return clases_db
 
-#post
 @app.post("/clases")
 def crear_clase(clase: Clase):
+    for c in clases_db:
+        if c["id"] == clase.id:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Ya existe una clase con id {clase.id}"
+            )
+
     clases_db.append(clase.dict())
     return {"mensaje": "Clase creada", "data": clase}
 
-#get id
 @app.get("/clases/{id}")
 def obtener_clase(id: int):
     for clase in clases_db:
@@ -38,10 +59,8 @@ def obtener_clase(id: int):
             return clase
     raise HTTPException(status_code=404, detail="Clase no encontrada")
 
-#filtro
 @app.get("/buscar")
 def buscar_clases(facultad: Optional[str] = None, activa: Optional[bool] = None):
-    
     resultados = clases_db
 
     if facultad:
